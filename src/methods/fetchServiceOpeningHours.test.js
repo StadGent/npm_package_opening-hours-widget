2ea6db16a3749f69c8307b5d90492faf1373170d
Helena Standaert <helena.standaert@gmail.com>
2018-11-27 16:00:12 +0100

    Fix JSON test for fetchServiceOpeningHours

--- src/methods/fetchServiceOpeningHours.test.js
+++ src/methods/fetchServiceOpeningHours.test.js
@@ -3,7 +3,6 @@
 import fetchServiceOpeningHours from './fetchServiceOpeningHours'
 jest.mock('../utils/request')
 
-
 describe('get fetchChannels for a service', () => {
   it('should return an HTML string', async () => {
     expect.assertions(1)
@@ -14,8 +13,8 @@ describe('get fetchChannels for a service', () => {
 
   it('should return an JSON object', async () => {
     expect.assertions(1)
-    const data = await fetchServiceOpeningHours('2690', {})
+    const data = await fetchServiceOpeningHours('2690', {}, 'json')
 
-    expect(typeof data).toBe('string')
+    expect(data).toBeInstanceOf(Array)
   })
 })
